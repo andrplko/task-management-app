08dcb3469deccbe1240d9fb5c23d7db439c3aead
andrplko <andrepoleshko@gmail.com>
2024-06-06 23:36:52 +0200

    feat: implement custom hook for updating query params

---
+++ frontend/src/hooks/useUpdateQueryParams.ts
@@ -0,0 +1,24 @@
+import { useSearchParams } from "react-router-dom";
+
+const useUpdateQueryParams = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const updateQueryParams = (newParams: Record<string, unknown>) => {
+    const currentParams = new URLSearchParams(searchParams);
+
+    Object.keys(newParams).forEach((key) => {
+      const value = newParams[key];
+      if (!value) {
+        currentParams.delete(key);
+      } else {
+        currentParams.set(key, String(value));
+      }
+    });
+
+    setSearchParams(currentParams);
+  };
+
+  return updateQueryParams;
+}
+
+export default useUpdateQueryParams;
