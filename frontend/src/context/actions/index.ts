619cd1b059cc99802a90cf2f8a43ef8bf61d3903
andrplko <andrepoleshko@gmail.com>
2024-06-12 23:00:33 +0200

    feat: implement form modal actions

---
+++ frontend/src/context/actions/index.ts
@@ -0,0 +1,18 @@
+import { Dispatch } from 'react';
+import { ActionType, Types } from '../reducers/formModalReducer';
+import { ModalMode } from '..';
+import { TodoItem } from '@types';
+
+const setIsOpen = (dispatch: Dispatch<ActionType>, payload: boolean) => {
+  dispatch({ type: Types.SET_IS_OPEN, payload });
+};
+
+const setMode = (dispatch: Dispatch<ActionType>, payload: ModalMode) => {
+  dispatch({ type: Types.SET_MODE, payload });
+};
+
+const setTodo = (dispatch: Dispatch<ActionType>, payload: TodoItem) => {
+  dispatch({ type: Types.SET_TODO, payload });
+};
+
+export { setIsOpen, setMode, setTodo };
