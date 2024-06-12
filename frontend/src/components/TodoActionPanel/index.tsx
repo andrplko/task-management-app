5187ce745f141dcf6e889a5ff7272945df341bea
andrplko <andrepoleshko@gmail.com>
2024-06-12 23:03:12 +0200

    feat: implement todo action panel and styles for it

---
+++ frontend/src/components/TodoActionPanel/index.tsx
@@ -0,0 +1,36 @@
+import { MouseEvent, PropsWithChildren } from 'react';
+import { ModalMode, useFormModalContext } from '@context';
+import { setIsOpen, setMode } from '@context/actions';
+import Button from '../ui/Button';
+import TodoFormModal from '../TodoFormModal';
+import PlusIcon from '@assets/plus-icon.svg?react';
+import styles from './TodoActionPanel.module.scss';
+
+const TodoActionPanel = ({ children }: PropsWithChildren) => {
+  const { state, dispatch } = useFormModalContext();
+  const { isOpen } = state;
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsOpen(dispatch, true);
+    setMode(dispatch, ModalMode.View);
+  };
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        {children}
+        <Button
+          type="button"
+          buttonClassName={styles.button}
+          onClick={handleClick}
+        >
+          <PlusIcon />
+        </Button>
+      </div>
+      {isOpen && <TodoFormModal />}
+    </>
+  );
+};
+
+export default TodoActionPanel;
