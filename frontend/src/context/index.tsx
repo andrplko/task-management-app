59152cb0c8f336e912ce5ee58f9f947af305d2b2
andrplko <andrepoleshko@gmail.com>
2024-06-12 22:58:37 +0200

    feat: implement form modal context

---
+++ frontend/src/context/index.tsx
@@ -0,0 +1,63 @@
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  Dispatch,
+  useReducer,
+  useMemo,
+} from 'react';
+import { ActionType, formModalReducer } from './reducers/formModalReducer';
+import { TodoItem } from '../types';
+
+export enum ModalMode {
+  View = 'view',
+  Edit = 'edit',
+}
+
+export interface InitialState {
+  isOpen: boolean;
+  mode: ModalMode;
+  todo: TodoItem;
+}
+
+const initialState: InitialState = {
+  isOpen: false,
+  mode: ModalMode.View,
+  todo: {
+    _id: '',
+    title: '',
+    description: '',
+    status: '',
+  },
+};
+
+const FormModalContext = createContext<{
+  state: InitialState;
+  dispatch: Dispatch<ActionType>;
+}>({
+  state: initialState,
+  dispatch: () => null,
+});
+
+const useFormModalContext = () => {
+  const context = useContext(FormModalContext);
+  if (!context) {
+    throw new Error(
+      'useFormModalContext must be used within a FormModalProvider'
+    );
+  }
+  return context;
+};
+
+const FormModalProvider = ({ children }: { children: ReactNode }) => {
+  const [state, dispatch] = useReducer(formModalReducer, initialState);
+  const value = useMemo(() => ({ state, dispatch }), [state, dispatch]);
+
+  return (
+    <FormModalContext.Provider value={value}>
+      {children}
+    </FormModalContext.Provider>
+  );
+};
+
+export { useFormModalContext, FormModalProvider };
