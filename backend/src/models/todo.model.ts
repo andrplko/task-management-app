8bdf0b4a782a7af1ef8a5e7a6b8c53a25a7f53b1
andrplko <andrepoleshko@gmail.com>
2024-07-18 10:09:16 +0200

    fix: change todo schema

--- backend/src/models/todo.model.ts
+++ backend/src/models/todo.model.ts
@@ -5,28 +5,32 @@ export default class Todo {
     public title: string,
     public description: number,
     public status: 'To Do' | 'In Progress' | 'Done',
-    public id?: ObjectId
+    public _id?: ObjectId
   ) {}
 }
 
 export const todoSchema = {
-  bsonType: 'object',
-  required: ['title', 'description', 'status'],
-  additionalProperties: false,
-  properties: {
-    _id: {},
-    title: {
-      bsonType: 'string',
-      description: "'title' is required and is a string",
-    },
-    description: {
-      bsonType: 'string',
-      description: "'description' is required and is a string",
-    },
-    status: {
-      enum: ['To Do', 'In Progress', 'Done'],
-      description:
-        "'status' is required and must be either To Do, In Progress, or Done",
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['_id', 'title', 'description', 'status'],
+      additionalProperties: false,
+      properties: {
+        _id: { bsonType: 'objectId' },
+        title: {
+          bsonType: 'string',
+          description: "'title' is required and is a string",
+        },
+        description: {
+          bsonType: 'string',
+          description: "'description' is required and is a string",
+        },
+        status: {
+          enum: ['To Do', 'In Progress', 'Done'],
+          description:
+            "'status' is required and must be either To Do, In Progress, or Done",
+        },
+      },
     },
   },
 };
