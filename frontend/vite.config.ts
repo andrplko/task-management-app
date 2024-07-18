a17a7501fde424d1600a42f0c9ce078500397001
andrplko <andrepoleshko@gmail.com>
2024-07-18 18:42:03 +0200

    feat: configure proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
@@ -7,40 +7,52 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), svgr()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 5173,
     },
-    host: true,
-    strictPort: true,
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@context': path.resolve(__dirname, './src/context'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        includePaths: [path.join(__dirname, 'src', 'styles')],
-        additionalData: `
-          @use 'variables' as *;
-          @use 'mixins' as *;
-          @use 'functions' as *;
-        `,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@constants': path.resolve(__dirname, './src/constants'),
+        '@context': path.resolve(__dirname, './src/context'),
+        '@services': path.resolve(__dirname, './src/services'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@types': path.resolve(__dirname, './src/types'),
+        '@utils': path.resolve(__dirname, './src/utils'),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          includePaths: [path.join(__dirname, 'src', 'styles')],
+          additionalData: `
+            @use 'variables' as *;
+            @use 'mixins' as *;
+            @use 'functions' as *;
+          `,
+        },
+      },
+    },
+  };
 });
