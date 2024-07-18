e0c876757b82ed73495aff15c11302eb35c625b6
andrplko <andrepoleshko@gmail.com>
2024-07-18 14:24:42 +0200

    feat: add radio button options

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,6 +1,21 @@
-export const BASE_URL = import.meta.env.VITE_API_URL;
-
 export const taskStatuses: string[] = ['All', 'To Do', 'In Progress', 'Done'];
+export const radioButtonOptions = [
+  {
+    value: 'To Do',
+    labelClassName: 'todo',
+    radioClassName: 'radio_todo',
+  },
+  {
+    value: 'In Progress',
+    labelClassName: 'in_progress',
+    radioClassName: 'radio_in_progress',
+  },
+  {
+    value: 'Done',
+    labelClassName: 'done',
+    radioClassName: 'radio_done',
+  },
+];
 export const sortOptions: string[] = [
   'ASC by status',
   'DESC by status',
