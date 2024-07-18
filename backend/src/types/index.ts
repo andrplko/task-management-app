16521a24b1c4b633abd958bb9acf6dfc4d4684cb
andrplko <andrepoleshko@gmail.com>
2024-07-18 10:36:55 +0200

    feat: implement DecodedToken interface

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,7 +1,12 @@
+import jwt from 'jsonwebtoken';
 import { Collection, Db, Document, MongoClient } from 'mongodb';
 
 export interface Connection {
   client: MongoClient;
   db: Db;
   collections: Record<string, Collection<Document>>;
+}
+
+export interface DecodedToken extends jwt.JwtPayload {
+  _id: string;
 }
