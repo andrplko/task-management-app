8ee052af058566f8427485d94058c62326fb0200
andrplko <andrepoleshko@gmail.com>
2024-07-18 14:42:28 +0200

    feat: add default options to query client and auth context provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { FormModalProvider } from './context/index.tsx';
+import { FormModalProvider } from './context/FormModalContext.tsx';
+import { AuthContextProvider } from '@context/AuthContext.tsx';
 import App from './App.tsx';
 import './index.scss';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <FormModalProvider>
-        <App />
-      </FormModalProvider>
+      <AuthContextProvider>
+        <FormModalProvider>
+          <App />
+        </FormModalProvider>
+      </AuthContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
