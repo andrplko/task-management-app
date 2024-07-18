dc8a0fe425997dc82ccb1b9a40b2e2d4bf3553ef
andrplko <andrepoleshko@gmail.com>
2024-07-18 10:09:40 +0200

    fix: change user schema

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -5,35 +5,37 @@ export default class User {
     public username: string,
     public email: string,
     public password: string,
-    public createdAt: string,
-    public id?: ObjectId
+    public createdAt: Date,
+    public _id?: ObjectId
   ) {}
 }
 
 export const userSchema = {
-  bsonType: 'object',
-  required: ['username', 'email', 'password'],
-  additionalProperties: false,
-  properties: {
-    _id: {
-      bsonType: 'objectId',
-    },
-    username: {
-      bsonType: 'string',
-      description: "'username' is required and is a string",
-    },
-    email: {
-      bsonType: 'string',
-      description: "'email' is required and is a string",
-    },
-    password: {
-      bsonType: 'string',
-      minLength: 8,
-      description:
-        "'password' is required and must be a string at least 8 characters long",
-    },
-    createdAt: {
-      bsonType: 'date',
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['_id', 'username', 'email', 'password', 'createdAt'],
+      additionalProperties: false,
+      properties: {
+        _id: { bsonType: 'objectId' },
+        username: {
+          bsonType: 'string',
+          description: "'username' is required and is a string",
+        },
+        email: {
+          bsonType: 'string',
+          description: "'email' is required and is a string",
+        },
+        password: {
+          bsonType: 'string',
+          minLength: 8,
+          description:
+            "'password' is required and must be a string at least 8 characters long",
+        },
+        createdAt: {
+          bsonType: 'date',
+        },
+      },
     },
   },
 };
