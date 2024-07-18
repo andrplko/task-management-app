c7b08092ddb9692450c6d407634ac7086e6bf84b
andrplko <andrepoleshko@gmail.com>
2024-07-18 14:16:38 +0200

    feat: add protected route and auth routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,8 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../components/ui/Layout';
 import HomePage from '../pages/HomePage';
 import TodoPage from '../pages/TodoPage';
+import SignInPage from '../pages/SignInPage';
+import SignUpPage from '../pages/SignUpPage';
 import RoutingErrorPage from '../pages/RoutingErrorPage';
 import Routes from '../constants/routes';
+import ProtectedRoute from '@components/ProtectedRoute';
 
 const router = createBrowserRouter([
   {
@@ -16,8 +19,21 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: Routes.TODO,
-        element: <TodoPage />,
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: Routes.TODO,
+            element: <TodoPage />,
+          },
+        ],
+      },
+      {
+        path: Routes.SIGN_IN,
+        element: <SignInPage />,
+      },
+      {
+        path: Routes.SIGN_UP,
+        element: <SignUpPage />,
       },
     ],
   },
