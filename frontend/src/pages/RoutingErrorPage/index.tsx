b4c421527e9a92a3f645b74aa75b83d01722e8a0
andrplko <andrepoleshko@gmail.com>
2024-07-19 11:05:47 +0200

    feat: add fallback ui for non-specific routing errors in RoutingErrorPage

--- frontend/src/pages/RoutingErrorPage/index.tsx
+++ frontend/src/pages/RoutingErrorPage/index.tsx
@@ -1,7 +1,8 @@
-import { Link, useRouteError, isRouteErrorResponse } from 'react-router-dom';
-import NotFoundImage from '@assets/not-found.svg?react';
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+import NavLinkButton from '@components/ui/NavLinkButton';
 import Layout from '@components/ui/Layout';
 import Routes from '@constants/routes';
+import NotFoundImage from '@assets/not-found.svg?react';
 import styles from './RoutingErrorPage.module.scss';
 
 const RoutingErrorPage = () => {
@@ -17,13 +18,17 @@ const RoutingErrorPage = () => {
             <NotFoundImage className={styles.image} />
           </div>
           <h2 className={styles.title}>{`${status} - ${statusText}`}</h2>
-          <Link to={Routes.HOME} className={styles.link}>
-            Go to the home page
-          </Link>
+          <NavLinkButton to={Routes.HOME}>Go to the home page</NavLinkButton>
         </div>
       </Layout>
     );
   }
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Something went wrong...</h2>
+    </div>
+  );
 };
 
 export default RoutingErrorPage;
