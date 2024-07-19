6e5550107f7428961ed937849918332997acde6e
andrplko <andrepoleshko@gmail.com>
2024-07-19 11:06:36 +0200

    feat: update header to conditionally render SearchBar and user info based on authentication state

--- frontend/src/components/ui/Header/index.tsx
+++ frontend/src/components/ui/Header/index.tsx
@@ -1,12 +1,25 @@
-import Wrapper from '../Wrapper';
+import { useAuthContext } from '@context/AuthContext';
+import UserNavigation from '@components/UserNavigation';
+import SearchBar from '@components/SearchBar';
+import ProfileIcon from '@assets/profile-icon.svg?react';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const { state } = useAuthContext();
+  const { user } = state;
+
   return (
     <header className={styles.header}>
-      <Wrapper>
-        <h1 className={styles.title}>Task Management App</h1>
-      </Wrapper>
+      {user && <SearchBar />}
+      <div className={styles.wrapper}>
+        {user && (
+          <div className={styles.user}>
+            <span className={styles.username}>Hi, {user.username}!</span>
+            <ProfileIcon className={styles.icon} />
+          </div>
+        )}
+        <UserNavigation />
+      </div>
     </header>
   );
 };
